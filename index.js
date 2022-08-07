f9d36fa0845481fea5c28fcd184e2b4a6e1f4d0e
ishaan-pare <ishaan.pare2020@vitbhopal.ac.in>
2022-08-07 22:03:11 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,18 @@
+//added reveal function which help us to reveal sections based on scrolls 
+//used for ux of webpage
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    var elementVisible = 150;
+
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+window.addEventListener("scroll", reveal);
